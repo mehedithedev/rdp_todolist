f68a5622695ad1fe7aca7c2341d91758dba71457
Mehedi Hasan <mehedi.developer@icloud.com>
2024-08-26 15:09:30 -0600

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:rdp_todolist/screens/home_page.dart';
 
-void main() {
+void main() async {
+  //Initialize Flutter bindings
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //Initialize Firebase with the current platform's default options
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MainApp());
 }
 
@@ -9,12 +20,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return  MaterialApp(
+      title: 'To-Do List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: HomePage(),
     );
   }
 }
