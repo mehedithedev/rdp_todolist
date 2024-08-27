5288241bb457a276bec4a7111b8165e1b847a7d2
Mehedi Hasan <mehedi.developer@icloud.com>
2024-08-27 15:35:27 -0600

    managed to have padding and margin around the todo items

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,7 +12,12 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   FirebaseFirestore db = FirebaseFirestore.instance;
 
-  final List<String> tasks = <String>[];
+  final List<String> tasks = <String>[
+    'Go home and sleep',
+    'Study for exam',
+    'Wake up',
+    'Keep Repeating'
+  ];
   final List<bool> checkboxes = List.generate(8, (index) => false);
   TextEditingController nameController = TextEditingController();
 
@@ -56,20 +62,101 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // Below is the App Bar
       appBar: AppBar(
         backgroundColor: Colors.blue,
         title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             SizedBox(
               height: 80,
-              child: Image.asset('assets/rdplogo.png'),
+              child: Image.asset(
+                  'assets/rdplogo.png'), // setting source of asset for the app bar icon
             ),
+            const // using const to impvove performance
             Text(
               'Daily Planner',
               style: TextStyle(
-                  fontFamily: 'Caveat', fontSize: 32, color: Colors.white),
+                  fontFamily:
+                      'Caveat' // using the Caveat font that we already added in pubspec.yaml file
+                  ,
+                  fontSize: 32,
+                  color: Colors.white),
             ),
+          ],
+        ),
+      ),
+
+      // Below is the body of my To-Do list app
+      body: Container(
+        // color: Colors.black,
+        child: Column(
+          children: [
+            TableCalendar(
+              calendarFormat: CalendarFormat
+                  .month, // defining how the calender will format dates, in this case month
+              headerVisible:
+                  true, // seeting the boolean value for app title bar
+              focusedDay: DateTime.now(), // setting today as the focused day
+              firstDay: DateTime(2023),
+              lastDay: DateTime(2025),
+            ),
+            ListView.builder(
+                shrinkWrap: true,
+                itemCount: tasks.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return SingleChildScrollView(
+                    child: Container(
+                      child: Container(
+                        margin: const EdgeInsets.only(top: 10),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: checkboxes[index]
+                                ? Colors.green.withOpacity(0.7)
+                                : Colors.blue.withOpacity(0.7)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Container(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Icon(!checkboxes[index]
+                                    ? Icons.manage_history
+                                    : Icons.playlist_add_check_circle),
+                                SizedBox(width: 18),
+                                Text('${tasks[index]}',
+                                    style: checkboxes[index]
+                                        ? TextStyle(
+                                            decoration:
+                                                TextDecoration.lineThrough,
+                                            fontSize: 20,
+                                            color:
+                                                Colors.black.withOpacity(0.5))
+                                        : TextStyle(fontSize: 20)),
+                                Row(
+                                  children: [
+                                    Checkbox(
+                                        value: checkboxes[index],
+                                        onChanged: (newValue) {
+                                          setState(() {
+                                            checkboxes[index] =
+                                                newValue!; // have to add ! as it can't be nullable
+                                          });
+                                          // To-Do: updateTaskComplettionStatus()
+                                        }),
+                                    const IconButton(
+                                        onPressed: null,
+                                        icon: Icon(Icons.delete))
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                })
           ],
         ),
       ),
